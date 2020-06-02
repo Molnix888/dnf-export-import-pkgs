bec44e1712f1592fa2c92410541c8ff9822cde26
Eugene Klimeshuk <17860027+Molnix888@users.noreply.github.com>
2020-06-02 03:02:57 +0300

    Add script and fix readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dnf-export-import-pkgs
-A script to export and restore list of packages for dnf package management system
+
+A script created to ease the process of restoring exactly the same set of packages after system re-installation or allowing to move it to another system
+
+Usage: ./dnf-export-import-pkgs.sh -o option
+    -o Option of script usage. It can either be 'export' or 'import'.
---
+++ dnf-export-import-pkgs.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+installedListToFileFunction () {
+    sudo dnf repoquery --installed | sort > $1 && echo "Package list successfully exported to $1." || echo "An error occured during operation."
+}
+
+helpFunction () {
+    echo "Usage: $0 -o option
+        -o Option of script usage. It can either be 'export' or 'import'."
+
+    exit 1
+}
+
+exportFunction () {
+    echo "Specify name for package list file (without extension):"
+
+    read filename
+
+    # Verifying if provided input is valid
+    if [ -z "$filename" ]
+    then
+        echo "Empty file name provided."
+    else
+        installedListToFileFunction "$filename.txt"
+    fi
+
+    exit 1
+}
+
+importFunction () {
+    echo "Specify file with package list (with extension):"
+
+    # Declaring variables for list files
+    read expected
+    actual=temp.txt
+
+    # Verifying if expected file exists, is readable and not empty
+    if [[ -f "$expected" && -r "$expected" && -s "$expected" ]]
+    then
+        # Creating actual pkgs list
+        installedListToFileFunction $actual
+
+        # Comparing actual list with expected and returning only the diff lines
+        diff=$(grep -Fxvf $expected $actual)
+
+        # Removing pkgs from diff list
+        sudo dnf remove $diff
+
+        # Installing pkgs from expected list
+        sudo dnf install $(cat $expected)
+
+        # Deleting temp actual list file
+        rm $actual && echo "$actual file successfully deleted." || echo "Can't delete $actual file."
+    else
+        echo "Provided file for packages import either not exists, not readable or is empty."
+    fi
+
+    exit 1
+}
+
+while getopts "o:" opt
+do
+    case "$opt" in
+        o ) option="$OPTARG";;
+        ? ) helpFunction;;
+    esac
+done
+
+# Verifying provided option parameter
+if [ -z "$option" ]
+then
+    echo "No parameters provided."
+    helpFunction
+elif [ "$option" = "export" ]
+then
+    exportFunction
+elif [ "$option" = "import" ]
+then
+    importFunction
+else
+    echo "Invalid parameter value provided."
+    helpFunction
+fi
