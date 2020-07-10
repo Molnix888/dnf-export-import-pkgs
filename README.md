7719b3d073630a5517c4c251ace1d8146b932c0d
Eugene Klimeshuk <17860027+Molnix888@users.noreply.github.com>
2020-07-11 01:55:02 +0300

    Perform general script optimization
    - Optimize comments and messages
    - Renaming to improve readability
    - Add check for file existance in export function
    - Add more information to help and readme

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 A script created to ease the process of restoring exactly the same set of packages after system re-installation or allowing to move it to another system.
 
-It uses only the package names and completely ignores the versions during operations.
+It uses only the package names and completely ignores the versions during operations. It is strongly recommended to use for import only generated file exported via this script.
 
 **Requires root privileges to run.**
 
     Usage: sudo ./dnf-export-import-pkgs.sh -o option
         -o Option of script usage. It can either be 'export' or 'import'.
+            export - Exports currently installed package names list (without versions and architecture) to plain text file.
+            import - Imports package names list from plain text file and installs exactly the same package set removing the ones not in provided list.
--- dnf-export-import-pkgs.sh
+++ dnf-export-import-pkgs.sh
@@ -1,64 +1,68 @@
 #!/bin/bash
 
-installedListToFileFunction () {
-    dnf repoquery --installed | sort | grep -oP "(^.+)(?=-[\d]+:.+)" | uniq -i > $1 && echo "Package list successfully exported to $1." || echo "An error occured during operation."
+installedPkgsToFileFunction () {
+    dnf repoquery --installed | sort | grep -oP "(^.+)(?=-[\d]+:.+)" | uniq -i > $1 && echo "Package list successfully exported to $1." || echo "Error occured during export operation."
 }
 
 helpFunction () {
     echo "Usage: sudo $0 -o option
-    -o Option of script usage. It can either be 'export' or 'import'."
+    -o Option of script usage. It can either be 'export' or 'import'.
+        export - Exports currently installed package names list (without versions and architecture) to plain text file.
+        import - Imports package names list from plain text file and installs exactly the same package set removing the ones not in provided list."
 
     exit 1
 }
 
 exportFunction () {
-    echo "Specify name for package list file (without extension):"
+    echo "Specify filename for package list (without extension):"
 
-    read filename
+    read name
 
-    # Verifying if provided input is valid
-    if [ -z "$filename" ]
+    # Verifying if input is valid
+    if [ -z "$name" ]
     then
-        echo "Empty file name provided."
+        echo "Empty filename provided."
+    elif [ -f "$name.txt" ]
+    then
+        echo "File with provided name already exists."
     else
-        installedListToFileFunction "$filename-$(uuidgen).txt"
+        installedPkgsToFileFunction "$name.txt"
     fi
 
     exit 1
 }
 
 importFunction () {
-    echo "Specify file with package list (with extension):"
+    echo "Specify path to file with package list to restore:"
 
-    # Declaring variables for list files
     read expected
-    actual=$(uuidgen).txt
 
-    # Verifying if expected file exists, is readable and not empty
+    # Verifying if file exists, readable and not empty
     if [[ -f "$expected" && -r "$expected" && -s "$expected" ]]
     then
-        # Creating actual pkgs list
-        installedListToFileFunction $actual
+        # Creating actual pkg list
+        actual=$(uuidgen).txt
+        installedPkgsToFileFunction $actual
 
-        # Comparing actual list with expected and returning only the diff lines
-        diff=$(grep -Fxvf $expected $actual)
+        # Comparing pkg lists and returning only the lines absent in expected list
+        toDelete=$(grep -Fxvf $expected $actual)
 
         # Removing pkgs from diff list
-        dnf remove $diff
+        dnf remove $toDelete
 
         # Installing pkgs from expected list
         dnf install $(cat $expected)
 
-        # Deleting temp actual list file
+        # Deleting temp file
         rm $actual && echo "$actual file successfully deleted." || echo "Can't delete $actual file."
     else
-        echo "Provided file for packages import either not exists, not readable or is empty."
+        echo "Provided file either not exists, not readable or is empty."
     fi
 
     exit 1
 }
 
-[[ $EUID -ne 0 ]] && echo "The script requires root privileges." && exit 1
+[[ $EUID -ne 0 ]] && echo "Script requires root privileges." && exit 1
 
 while getopts "o:" opt
 do
