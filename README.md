ebd881023b21d0e6043a2b42c270120ce221c849
Eugene Klimeshuk <17860027+Molnix888@users.noreply.github.com>
2020-07-17 01:54:37 +0300

    Refactor script and improve documentation
    - Refactor script to syntax closer to POSIX-compatible
    - Remove misused exit codes
    - Improve code style
    - Replace some global variables with local
    - Improve internal and external documentation

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # dnf-export-import-pkgs
 
-A script created to ease the process of restoring exactly the same set of packages after system re-installation or allowing to move it to another system.
+A script created to ease the process of restoring the same set of packages after system re-installation or for moving it to another system.
 
-It uses only the package names and completely ignores the versions during operations. It is strongly recommended to use for import only generated file exported via this script.
+It uses only the package names and ignores the versions during operations. It is recommended for import operation to use only files exported via this script.
 
 **Requires root privileges to run.**
 
-    Usage: sudo ./dnf-export-import-pkgs.sh -o option
-        -o Option of script usage. It can either be 'export' or 'import'.
-            export - Exports currently installed package names list (without versions and architecture) to plain text file.
-            import - Imports package names list from plain text file and installs exactly the same package set removing the ones not in provided list.
+## Use
+
+    $ sudo ./dnf-export-import-pkgs.sh
+    Usage: sudo ./dnf-export-import-pkgs.sh [-o <export|import>]
+    
+        Arguments:
+        export - Exports currently installed package names (without version and architecture) to plain text file.
+        import - Imports package names from plain text file and installs exactly the same package set removing the ones not in the list.
--- dnf-export-import-pkgs.sh
+++ dnf-export-import-pkgs.sh
@@ -1,85 +1,70 @@
 #!/bin/bash
 
-installedPkgsToFileFunction () {
-    if [ -f "$1" ]
-    then
-        echo "File $1 already exists."
-        exit 1
+installedPkgsToFileFunction() {
+    if [ -f "$1" ]; then
+        echo "File $1 already exists." && exit 1
     else
-        dnf repoquery --installed | sort | grep -oP "(^.+)(?=-[\d]+:.+)" | uniq -i > $1 && echo "Package list successfully exported to $1." || echo "Error occured during export operation."
+        dnf repoquery --installed | sort | grep -oP "(^.+)(?=-[\d]+:.+)" | uniq -i > $1 && echo "Package list successfully exported to $1." || ( echo "Error occured during export operation." && exit 1 )
     fi
 }
 
-helpFunction () {
-    echo "Usage: sudo $0 -o option
-    -o Option of script usage. It can either be 'export' or 'import'.
-        export - Exports currently installed package names list (without versions and architecture) to plain text file.
-        import - Imports package names list from plain text file and installs exactly the same package set removing the ones not in provided list."
+helpFunction() {
+    echo "Usage: sudo $0 [-o <export|import>]
+
+    Arguments:
+    export - Exports currently installed package names (without version and architecture) to plain text file.
+    import - Imports package names from plain text file and installs exactly the same package set removing the ones not in the list."
 
     exit 1
 }
 
-exportFunction () {
-    echo "Specify filename for package list (without extension):"
+exportFunction() {
+    read -p "Specify filename for package list (without extension): " name
 
-    read name
-
-    if [ -z "$name" ]
-    then
-        echo "Empty filename provided."
+    if [ -z "$name" ]; then
+        echo "Empty filename provided." && exit 1
     else
         installedPkgsToFileFunction "$name.txt"
     fi
-
-    exit 1
 }
 
-importFunction () {
-    echo "Specify path to file with package list to restore:"
+importFunction() {
+    read -p "Specify path to file with package list to restore: " expected
 
-    read expected
-
-    if [[ -f "$expected" && -r "$expected" && -s "$expected" ]]
-    then
-        actual=$(uuidgen).txt
+    if [ -f "$expected" ] && [ -r "$expected" ] && [ -s "$expected" ]; then
+        local actual=$(uuidgen).txt
         installedPkgsToFileFunction $actual
 
         # Comparing pkg lists and returning only the lines absent in expected list
-        toDelete=$(grep -Fxvf $expected $actual)
+        local toDelete=$(grep -Fxvf $expected $actual)
 
         dnf remove $toDelete
 
         dnf install $(cat $expected)
 
-        rm $actual && echo "$actual file successfully deleted." || echo "Can't delete $actual file."
+        rm $actual && echo "$actual file successfully deleted." || ( echo "Can't delete $actual file." && exit 1 )
     else
-        echo "Provided file either not exists, not readable or is empty."
+        echo "File not exists, not readable or is empty." && exit 1
     fi
-
-    exit 1
 }
 
-[[ $EUID -ne 0 ]] && echo "Script requires root privileges." && exit 1
+if [ $EUID -ne 0 ]; then
+    echo "Script requires root privileges." && exit 1
+fi
 
-while getopts "o:" opt
-do
+while getopts "o:" opt; do
     case "$opt" in
-        o ) option="$OPTARG";;
+        o ) arg="$OPTARG";;
         ? ) helpFunction;;
     esac
 done
 
-if [ -z "$option" ]
-then
-    echo "No parameters provided."
+if [ -z "$arg" ]; then
     helpFunction
-elif [ "$option" = "export" ]
-then
+elif [ "$arg" = "export" ]; then
     exportFunction
-elif [ "$option" = "import" ]
-then
+elif [ "$arg" = "import" ]; then
     importFunction
 else
-    echo "Invalid parameter value provided."
-    helpFunction
+    echo "Invalid argument." && helpFunction
 fi
