7fad7d4c1c4cd4cc6e867e1e922d012e8022cbcf
Eugene Klimeshuk <17860027+Molnix888@users.noreply.github.com>
2022-01-23 14:50:51 +0100

    Optimize script run time

--- dnf-export-import-pkgs.sh
+++ dnf-export-import-pkgs.sh
@@ -14,7 +14,7 @@ get_help() {
 
 pkg_list_to_file() {
     if [ -f "$1" ]; then
-        echo "$1 already exists." && get_help
+        echo "$1 already exists."
     else
         (dnf repoquery --installed | sort | grep -oP "(^.+)(?=-[\d]+:.+)" | uniq -i >"$1" && echo "Package list successfully exported to $1.") || (echo "Error occurred during exporting to $1." && exit 1)
     fi
@@ -22,36 +22,48 @@ pkg_list_to_file() {
 
 export_pkgs() {
     if [ -z "$1" ]; then
-        echo "Empty filepath." && get_help
+        echo "Empty filepath."
     else
         pkg_list_to_file "$1"
     fi
+}
+
+# Compares 2 files returning lines absent in 1.
+get_delta() {
+    echo grep -Fxvf "$1" "$2"
+}
+
+remove_file() {
+    (rm "$1" && echo "$1 successfully deleted.") || (echo "Error occurred during deletion of $1." && exit 1)
 }
 
 import_pkgs() {
     if [ -f "$1" ] && [ -r "$1" ] && [ -s "$1" ]; then
         local actual
         actual=$(uuidgen)
+
         pkg_list_to_file "$actual"
 
-        # Comparing pkg lists and returning only the lines absent in received list
         local to_delete
-        to_delete=$(grep -Fxvf "$1" "$actual")
+        to_delete=$(get_delta "$1" "$actual")
 
         for pkg in $to_delete; do
             dnf remove -y "$pkg"
         done
 
+        remove_file "$actual"
+        pkg_list_to_file "$actual"
+
         local to_install
-        to_install=$(cat "$1")
+        to_install=$(get_delta "$actual" "$1")
 
         for pkg in $to_install; do
             dnf --setopt=install_weak_deps=False install -y "$pkg"
         done
 
-        (rm "$actual" && echo "$actual successfully deleted.") || (echo "Error occurred during deletion of $actual." && exit 1)
+        remove_file "$actual"
     else
-        echo "File not exists, not readable or is empty." && get_help
+        echo "File not exists, not readable or is empty."
     fi
 }
 
